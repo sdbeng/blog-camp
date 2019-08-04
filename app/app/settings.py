e17f55feed4d1786fe3968cff512b614274ba0a9
Sergio Barrera <sdbeng@gmail.com>
2019-08-04 00:17:31 -0700

    add Post model, superuser, test posts to db

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    # 'blog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Los Angeles'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
